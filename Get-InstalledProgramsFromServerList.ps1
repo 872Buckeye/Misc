a59989c91cad476835803d24e37e70c834b34390
872Buckeye <51181105+872Buckeye@users.noreply.github.com>
2020-01-15 20:14:19 -0500

    Create Get-InstalledProgramsFromServerList.ps1

---
+++ Get-InstalledProgramsFromServerList.ps1
@@ -0,0 +1,38 @@
+
+# One Computer Per Line
+$myServers = @"
+JAX1EXCH01
+JAX1EXCH02
+JAX1EXCH03
+"@ -split '\r?\n'.Trim()
+
+# One Search Per Line
+$mySearch = @"
+google
+7zip
+vmware
+"@ -split '\r?\n'.Trim()
+
+$MyLog = @()
+
+foreach ($u in $myServers) {
+
+    foreach ($v in $mySearch) {
+
+        write-host $u $v
+
+        foreach ($w in (Get-WmiObject -ComputerName $u -Class win32_product | ?{$_.Name -like '*$v*'})) {
+
+            write-host $w.Name "Found"
+            $myItem = New-Object PSObject
+            $myItem | Add-Member -type NoteProperty -Name 'Server' -Value $u
+            $myItem | Add-Member -type NoteProperty -Name 'Software' -Value $w.Name
+            $myItem | Add-Member -type NoteProperty -Name 'Version' -Value $w.version
+            $MyLog += $myItem        
+
+        }
+
+    }
+
+}
+
